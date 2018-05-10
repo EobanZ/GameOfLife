f5debc9341bb71b434c88ebe4ffecc074d178e53
Jack Pioneer <zwick.fabi@googlemail.com>
2018-05-10 18:38:53 +0200

    first

---
+++ Game of Life/Assets/Scripts/Cell.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    bool? isAlive = true;
+    bool? nextState = null;
+    float lifeTime = 0; //the amount of seconds this cell is alive
+    Vector2 position;
+    [SerializeField] float maxlifeTime = 10f; //After this amount of seconds the Cell is the gradientColor
+    [SerializeField] Color aliveColor = Color.white;
+    [SerializeField] Color deadColor = Color.black;
+    [SerializeField] Color gradientColor = Color.red;
+
+#region private methods
+
+    private void Start()
+    {
+        UpdateSprites(); //Updates the Sprites Colors once the game is started
+    }
+
+    private void Update()
+    {
+        if(isAlive == true)
+            lifeTime += Time.deltaTime;
+    }
+
+    private int CountLivingNeighbors() { return 0; }
+
+    private bool ChooseNextState(int livingNeighbors) { return true; }
+
+    private void UpdateSprites()
+    {
+        if (isAlive == false)
+        {
+            GetComponent<SpriteRenderer>().color = deadColor;
+            lifeTime = 0;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.Lerp(aliveColor, gradientColor, Mathf.Clamp01(lifeTime/maxlifeTime));
+        }
+    }
+
+    private void ApplyNextGeneration()
+    {
+        if(nextState != null)
+        {
+            isAlive = nextState;
+            nextState = null;
+            
+        }
+        UpdateSprites();
+    }
+
+#endregion
+
+#region public methods
+
+    public void SetFieldPosition(int x, int y) { position = new Vector2(x, y);}
+
+    public void SetAlive(bool b)
+    {
+        if (b)
+            isAlive = true;
+        else
+            isAlive = false;
+    }
+
+    public void UpdateCell()
+    {
+        ChooseNextState(CountLivingNeighbors());
+        ApplyNextGeneration();
+    }
+
+#endregion
+
+}
---
+++ Game of Life/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : GenericSingletonClass<GameManager> {
+
+    float timer = 0;
+    float refreshTime = 1/30;
+    public int fieldSizeX, fieldSizeY;
+    GameObject[,] cells;
+    Camera camera;
+
+    public GameObject cellPrefab;
+    public GameObject Field;
+    public GameObject[,] Cells { get { return cells; } }
+    
+
+
+    void Start () {
+        camera = Camera.main; 
+        fieldSizeY = Mathf.RoundToInt(fieldSizeX * (1 / camera.aspect));
+        cells = new GameObject[fieldSizeX,fieldSizeY];
+
+        PositionCamera();
+        BuildField(true); 
+	}
+	
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= refreshTime)
+        {
+            UpdateField();
+            timer = 0;
+        }
+        
+	}
+
+    void PositionCamera()
+    {
+          
+        camera.transform.position = new Vector3((float)fieldSizeX / 2 - 0.5f, (float)fieldSizeY / 2 - 0.5f, -1);
+        camera.orthographicSize = fieldSizeX >= fieldSizeY? fieldSizeY/2 : fieldSizeX/2 ;
+    }
+
+    void BuildField(bool random)
+    {
+       
+        for (int x = 0; x < fieldSizeX; x++)
+        {
+            for (int y = 0; y < fieldSizeY; y++)
+            {
+                GameObject go = (GameObject)Instantiate(cellPrefab, new Vector3(x, y, 0), Quaternion.identity,Field.transform);
+                go.GetComponent<Cell>().SetFieldPosition(x, y);
+                cells[x, y] = go;
+                if (random)
+                {
+                    int i = (int)Random.Range(0, 1.99f);
+                    bool b = i == 0 ? false : true;
+                    go.GetComponent<Cell>().SetAlive(b);
+                }
+            }
+        }
+    }
+
+    //Updates each cell in the Field
+    void UpdateField()
+    {
+        for (int x = 0; x < fieldSizeX; x++)
+        {
+            for (int y = 0; y < fieldSizeY; y++)
+            {
+
+                cells[x, y].GetComponent<Cell>().UpdateCell();
+               
+            }
+        }
+    }
+
+    
+}
