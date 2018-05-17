37dbcc85141737e41758ea209b79d11074fe3551
DESKTOP-Grassau\Fabi <grassauPc@mail1a.de>
2018-05-17 16:19:01 +0200

    MapContainerSO und Map Klasse hinzugefügt. + UI bissl erweitert

---
+++ Game of Life/Assets/Scripts/Map.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map
+{
+    string name;
+    int width;
+    int height;
+    bool[,] field;
+
+    public string Name { get { return name; } }
+
+    Map(bool[,] Field, int width, int height) { createMap(Field,width,height); }
+
+    void createMap(bool[,] Field, int width, int height) { this.field = Field; this.width = width; this.height = height; }
+
+}
---
+++ Game of Life/Assets/Scripts/SO Scripts/MapContainerSO.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewMapContainer", menuName = "MapContainer")]
+public class MapContainerSO : ScriptableObject
+{
+    Dictionary<string, Map> maps = new Dictionary<string, Map>();
+
+    public Dictionary<string, Map> Maps { get { return maps; } }
+
+    public void AddMap(Map m)
+    {
+        if(!maps.ContainsKey(m.Name))
+        maps.Add(m.Name, m);
+    }
+
+    public void removeMap(string mapName)
+    {
+        if (maps.ContainsKey(mapName))
+            maps.Remove(mapName);
+    }
+
+    
+
+}
